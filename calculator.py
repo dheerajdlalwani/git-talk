6d64aca5ec02bbb86df424e14c53ca223cd4890e
Dheeraj Lalwani <dheeraj@heymarvin.com>
2025-07-12 12:20:25 +0530

    add a function for division

--- calculator.py
+++ calculator.py
@@ -10,6 +10,13 @@ def sub(a, b):
 def mul(a, b):
     return a*b
 
+def div(a, b):
+    if b != 0:
+        return a/b
+    else:
+        return None
+
+
 
 def series_addition(some_list):
     total = 0
